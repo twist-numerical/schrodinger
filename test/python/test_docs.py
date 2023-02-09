eac252792970554caea4d79e5002e17f50930d5f
Toon Baeyens <toon.baeyens@gmail.com>
2023-02-09 18:26:17 +0100

    point doctest to the correct package

--- test/python/test_docs.py
+++ test/python/test_docs.py
@@ -1,9 +1,18 @@
 import strands
+from strands import Schrodinger2D, Rectangle
 import sys
 import doctest
+from math import pi
 
-failures, test_count = doctest.testmod(strands)
+s = Schrodinger2D(lambda x, y: 0, Rectangle(0,pi,0,pi))
+eigs = s.eigenvalues(1)
+assert abs(eigs[0] - 2) < 1e-6, "Wrong result from zero potential."
+
+
+failures, test_count = doctest.testmod(strands.strands)
 
 assert test_count > 0, "No tests were found"
+
+print(f"{test_count} tests, {failures} failures")
 
 sys.exit(1 if failures else 0)
